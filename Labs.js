064de260ba776b8a942e4dcea5615395a16fa323
XiomaraJimenez <xiomararosado.0491@hotmail.com>
2020-02-19 06:54:10 +0000

    ‘commit’

---
+++ Labs.js
@@ -0,0 +1,30 @@
+var firstName = 'Elshy Xiomara';
+var faveColour= 'Blue';
+var faveFood = 'Avocado Hummus'; //value for favefood
+var city = 'Dublin'; 
+console.log(firstName+' '+faveColour+ ' '+faveFood+ ' '+city); //It will show me the valor of each var, I've added speace betweet each var
+
+var x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log(x) //show the value of x
+
+var x = 'Laurel';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+
+console.log();
+phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.' ;
+endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see! ';
+one = 'Red';
+two ='orange';
+three = 'yellow';
+four = 'green';
+five = 'blue';
+six = 'indigo';
+seven = 'violet';
+
+console.log (phrase+' '+one+', '+two+', '+three+', '+four+', '+five+', '+six+', '+seven+' '+endingPhrase);
